5161e795c77fbc527f51ea80e83c1c7fd39a5c00
zhangdapeng520 <1156956636@qq.com>
2024-08-04 15:37:54 +0800

    新增：初始代码

---
+++ src/assets/js/tooltip.js
@@ -0,0 +1,11 @@
+import bootstrap from "bootstrap/dist/js/bootstrap";
+
+// initialization of Tooltips
+export default function setTooltip() {
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+}
